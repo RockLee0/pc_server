267092a2c51c180fafe960b9859d8573fc7b97cc
Rocklee0 <mshakil3847@gmail.com>
2023-08-06 13:57:44 +0600

    id problem fixed

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc7xgzn.mongodb.net/?retryWrites=true&w=majority`
+
+const uri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nc7xgzn.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,13 +20,47 @@ const run = async () => {
   try {
     const db = client.db('pc_parts');
     const productCollection = db.collection('products');
+ 
+ 
+ 
+ 
+    app.get('/products/:id', async (req, res) => {
+      const idValue = req.params.id;
+      console.log(...idValue);
+    
+      try {
+        const product = await productCollection.findOne({ id:idValue });
+        console.log(product);
+        if (!product) {
+          return res.status(404).send({ status: false, message: 'Product not found' });
+        }
+    
+        res.send({ status: true, data: product });
+      } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).send({ status: false, message: 'An error occurred' });
+      }
+    });
+
+
 
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
       const product = await cursor.toArray();
-
-      res.send({ status: true, data: product });
+      res.send({ status: true, data: product});
     });
+
+
+    app.get('/products/:Category', async (req, res) => {
+      const propertyValue = req.params.Category;
+      
+      const cursor = await productCollection.find({ Category: propertyValue});
+      const product = await cursor.toArray();
+      res.send({ status: true, data: product});
+      
+    });
+
+
   } finally {
   }
 };
